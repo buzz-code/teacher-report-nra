a42f91aba53964e1aa4c9bd6f0a8685bcd0202ba
buzz-code <buzz-code@users.noreply.github.com>
2025-09-18 18:32:23 +0000

    fix: Update applicationId and service name for teacher report configuration

--- client/src/openopserve.config.ts
+++ client/src/openopserve.config.ts
@@ -3,9 +3,9 @@ import packageJson from '../package.json';
 
 const options = {
     clientToken: '32363366626236332d333131632d343665642d616433372d316332366563386566313330',
-    applicationId: 'event-yoman-online',
+    applicationId: 'teacher-yoman-online',
     site: 'api.openobserve.ai',
-    service: 'event-frontend',
+    service: 'teacher-frontend',
     env: location.href.includes('dev') || location.href.includes('localhost') ? 'development' : 'production',
     version: packageJson.version,
     organizationIdentifier: 'hadasa_organization_50794_yN95E6myGED9AWO',
@@ -13,4 +13,4 @@ const options = {
     apiVersion: 'v1',
 };
 
-connectToOpenobserve(options);
+// connectToOpenobserve(options);
