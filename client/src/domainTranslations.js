ddd1e3ef7d354d3805eb72aa918a8a71161f90ba
buzz-code <buzz-code@users.noreply.github.com>
2025-11-08 19:23:36 +0000

    feat: add Hebrew translation for teacher reference ID in domainTranslations

--- client/src/domainTranslations.js
+++ client/src/domainTranslations.js
@@ -13,6 +13,7 @@ export default {
             fields: {
                 ...generalResourceFieldsTranslation,
                 tz: 'תעודת זהות',
+                teacherReferenceId: 'מורה',
             }
         },
         teacher: {
