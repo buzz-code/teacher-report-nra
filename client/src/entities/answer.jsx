9ff1f6b369cb99febeeafb5648268331b66da019
Hadasa Schechter <72219643+buzz-code@users.noreply.github.com>
2026-02-11 11:52:29 +0000

    fix(report): update questionId reference to questionReferenceId in Datagrid

--- client/src/entities/answer.jsx
+++ client/src/entities/answer.jsx
@@ -95,7 +95,7 @@ const Datagrid = ({ isAdmin, children, ...props }) => {
             {isAdmin && <TextField source="id" />}
             {isAdmin && <ReferenceField source="userId" reference="user" />}
             <ReferenceField source="teacherReferenceId" reference="teacher" />
-            <ReferenceField source="questionId" reference="question">
+            <ReferenceField source="questionReferenceId" reference="question">
               <TextField source="content" />
             </ReferenceField>
             <ReferenceField source="salaryReportId" reference="salary_report" />
