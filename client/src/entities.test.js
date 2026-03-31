cc5e3fae61b1d941b92689a7ecb762e8206d92dd
Hadasa Schechter <72219643+buzz-code@users.noreply.github.com>
2026-03-31 18:31:00 +0000

    refactor: move entities.test.js into shared module; delete per-project copy

--- client/src/entities.test.js
+++
@@ -1,5 +0,0 @@
-// Smoke-tests every React Admin resource page. Add a <Resource> to App.jsx and it's auto-tested.
-import { createResourceTests } from '@shared/utils/testing/createResourceTests';
-import App from './App';
-
-createResourceTests(App);
