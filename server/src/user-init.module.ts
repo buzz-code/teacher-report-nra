95c06fa003355dc6ad18d32b9c6150de1fed60a8
Hadasa Schechter <72219643+buzz-code@users.noreply.github.com>
2026-05-03 10:44:44 +0000

    feat: migrate to BaseNraAppModule.forRoot, remove no-op user-init (Day 7)

--- server/src/user-init.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UserInitializationService } from './user-initialization.service';
-
-@Module({
-  providers: [UserInitializationService],
-  exports: [UserInitializationService],
-})
-export class UserInitModule {}
