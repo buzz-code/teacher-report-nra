bd3f1ce26ec8ad69e1967226315593aafbd5d526
Hadasa Schechter <72219643+buzz-code@users.noreply.github.com>
2026-05-10 19:20:36 +0000

    fix: add setupFilesAfterEnv to client jest.config.js so @testing-library/jest-dom matchers load

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,6 +1,7 @@
 const base = require('./shared/config/jest.base');
 module.exports = {
     ...base,
+    setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
     coverageThreshold: {
         global: {
             statements: 42,
