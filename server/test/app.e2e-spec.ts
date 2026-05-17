badd3b33b14eb35e57eb34ae18a76c073478c072
Hadasa Schechter <72219643+buzz-code@users.noreply.github.com>
2026-05-17 20:00:58 +0000

    test: use shared e2e factory, remove duplicate helpers

--- server/test/app.e2e-spec.ts
+++ server/test/app.e2e-spec.ts
@@ -1,23 +1,4 @@
-import { createTestApp, TestAppHelper } from './helpers/test-app.helper';
-import { HttpTestUtils } from './helpers/test-utils';
+import { AppModule } from 'src/app.module';
+import { createSharedAppE2eTests } from '@shared/utils/testing/e2e/app-e2e.shared';
 
-describe('AppController (e2e)', () => {
-  let testApp: TestAppHelper;
-  let httpUtils: HttpTestUtils;
-
-  beforeAll(async () => {
-    testApp = createTestApp();
-    const app = await testApp.initializeApp();
-    httpUtils = new HttpTestUtils(app);
-  });
-
-  afterAll(async () => {
-    await testApp.cleanup();
-  });
-
-  describe('Health Check', () => {
-    it('/ (GET) should return Hello World!', async () => {
-      return httpUtils.get('/').expect(200).expect('Hello World!');
-    });
-  });
-});
+createSharedAppE2eTests(AppModule);
